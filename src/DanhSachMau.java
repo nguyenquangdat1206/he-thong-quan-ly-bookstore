f10f6329f925dcc8a1df57d7f3be05745a650ce0
nguyenquangdat1206 <nguyenquangdat898@gmail.com>
2026-09-21 08:38:31 +0700

    tạo file mẫu cho ae vào đọc để làm
    Implement basic functionality for DanhSachMau class including add and remove methods.

---
+++ src/DanhSachMau.java
@@ -0,0 +1,49 @@
+// ĐÂY LÀ CODE MẪU CHO CÁC BẠN LÀM CLASS DANH SÁCH (COPY VÀ SỬA TÊN CLASS)
+public class DanhSachMau implements IQuanLy {
+    private Object[] ds; // Đổi Object thành MatHang, NhanVien, HoaDon... tùy người làm
+    private int soLuong;
+
+    public DanhSachMau() {
+        ds = new Object[1000]; // Khởi tạo mảng to cố định
+        soLuong = 0;           // Lúc đầu mảng rỗng
+    }
+
+    @Override
+    public void them() {
+        // TODO: Viết code nhập thông tin đối tượng mới ở đây
+        // Giả sử đã tạo được đối tượng mới tên là 'obj'
+        Object obj = new Object(); 
+        
+        if (soLuong < ds.length) {
+            ds[soLuong] = obj;
+            soLuong++;
+            System.out.println("Thêm thành công!");
+        } else {
+            System.out.println("Danh sách đã đầy!");
+        }
+    }
+
+    @Override
+    public void xoa(String ma) {
+        int viTri = -1;
+        // 1. Tìm vị trí của phần tử cần xóa
+        for (int i = 0; i < soLuong; i++) {
+            // TODO: Viết logic so sánh mã ở đây. Giả sử tìm thấy ở vị trí i:
+            // if (ds[i].getMa().equals(ma)) { viTri = i; break; }
+        }
+
+        // 2. Thuật toán dồn mảng để xóa
+        if (viTri != -1) {
+            for (int i = viTri; i < soLuong - 1; i++) {
+                ds[i] = ds[i + 1]; // Đẩy phần tử phía sau lên trước
+            }
+            ds[soLuong - 1] = null; // Xóa dữ liệu rác ở cuối
+            soLuong--;
+            System.out.println("Xóa thành công!");
+        } else {
+            System.out.println("Không tìm thấy mã cần xóa!");
+        }
+    }
+    
+    // Các hàm nhap(), xuat(), sua() tự viết logic tương tự...
+}
